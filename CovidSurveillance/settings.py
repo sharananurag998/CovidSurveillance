2112358af07e6e7f8fd8365b1a7dd5ef1a3c41d3
= <abdulmateen075@gmail.com>
2020-05-06 13:10:31 +0530

    updated settings.py to work with unix based systems

--- CovidSurveillance/settings.py
+++ CovidSurveillance/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CovidSurveillance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['CovidSurveillance/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, 'CovidSurveillance/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
