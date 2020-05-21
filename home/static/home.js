6ecb661a00848c2e82bf7785a3c75f343c792a9e
Aniketh896 <aniketh.hotagi@gmail.com>
2020-05-21 23:55:36 +0530

    Made Homepage table functioning

--- home/static/home.js
+++ home/static/home.js
@@ -1,34 +1,21 @@
+json_url = "https://api.covid19india.org/data.json";
 
-document.addEventListener("DOMContentLoaded", function(){
-
-    google.script.run.withSuccessHandler(generateTable).getTableData();
-
-    
-
+$(document).ready(function(){
+    $.ajax({
+        url:json_url,
+        dataType:"json",
+        success:function(data){
+            $(data.statewise).each(function(index, value){
+                var record = "";
+                record += "<tr>";
+                record += "<td>"+value.state+"</td>";;
+                record += "<td>"+value.confirmed+"</td>";
+                record += "<td>"+value.active+"</td>";
+                record += "<td>"+value.recovered+"</td>";
+                record += "<td>"+value.deaths+"</td>";
+                record += "</tr>";
+                $("table").append(record);
+            });
+        }
     });
-
-function generateTable(dataArray){
-    var tbody = document.getElementById("table-body");
-
-    dataArray.forEach(function(r){
-
-        var row = document.createElement("tr");
-        var col1 = document.createElement("td");
-        col1.textContent = r[0];
-        var col2 = document.createElement("td");
-        col2.textContent = r[1];
-        var col3 = document.createElement("td");
-        col3.textContent = r[2];
-        var col4 = document.createElement("td");
-        col4.textContent = r[3];
-        var col5 = document.createElement("td");
-        col5.textContent = r[4];
-        row.appendChild(col1);
-        row.appendChild(col2);
-        row.appendChild(col3);
-        row.appendChild(col4);
-        row.appendChild(col5);
-        tbody.appendChild(row);
-
-    });   
-}
+});
