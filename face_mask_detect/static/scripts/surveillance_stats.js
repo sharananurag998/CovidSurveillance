f195b02727681a3d56f0802a7bf640e2c791f099
Abdul Mateen <abdulmateen075@gmail.com>
2020-05-23 02:26:56 +0530

    moved css styles to stylesheets

--- face_mask_detect/static/scripts/surveillance_stats.js
+++ face_mask_detect/static/scripts/surveillance_stats.js
@@ -66,7 +66,7 @@ if (typeof(EventSource) !== 'undefined') {
 
         if (!videoPlayback) {
             video.play();
-            video.playbackRate = 0.15;
+            video.playbackRate = 0.5;
             videoPlayback = true;
         }
     }
