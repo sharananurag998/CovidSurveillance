372d91fdbf0a153ef536235b2f5f1ae7291a3dd1
Anurag Sharan <34830688+sharananurag998@users.noreply.github.com>
2020-05-31 22:51:19 +0530

    Fix streaming issues on windows
    (DEBUG = FALSE)

--- CovidSurveillance/settings.py
+++ CovidSurveillance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_tm^=d9wnyhvhd-emscrsa9c%qxxcwe&*yg_0sg^wjx8i^+^2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
