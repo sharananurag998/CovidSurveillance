6b6532741a5ce17cfd6b5ed93bc861135cf2b240
Anurag Sharan <34830688+sharananurag998@users.noreply.github.com>
2020-05-31 23:10:50 +0530

    Revert "Update settings.py"
    This reverts commit 822fcd3d3a20f3b6ab1d052816c126e16a0fab0f.

--- CovidSurveillance/settings.py
+++ CovidSurveillance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_tm^=d9wnyhvhd-emscrsa9c%qxxcwe&*yg_0sg^wjx8i^+^2$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = []
 
 
 # Application definition
