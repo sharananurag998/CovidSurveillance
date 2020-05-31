107b80a7ca08e9189299af53e6cea0bbbe4c6639
Aniketh Hotagi <aniketh.hotagi@gmail.com>
2020-05-31 23:15:18 +0530

    Revert "Homepage is back to normal"
    This reverts commit a9e10f4a99b4436df30ce6b41c3fc3fe41c3386e.

--- CovidSurveillance/settings.py
+++ CovidSurveillance/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_tm^=d9wnyhvhd-emscrsa9c%qxxcwe&*yg_0sg^wjx8i^+^2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
